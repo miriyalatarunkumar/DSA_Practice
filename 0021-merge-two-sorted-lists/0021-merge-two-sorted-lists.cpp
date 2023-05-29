8e108a3a9cc983d8e777e5bb1300c19fea7ca6d1
MIRIYALA TARUN KUMAR <83545474+miriyalatarunkumar@users.noreply.github.com>
2023-05-29 20:28:35 +0530

    Time: 4 ms (92.73%), Space: 14.8 MB (34.88%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,34 +11,32 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == nullptr) return list2;
+        if(list2 == nullptr) return list1;
         ListNode* Newhead;
         ListNode* tail;
-        if(list1&&list2){
-            if(list1->val<list2->val){
-                Newhead=list1;
-                tail = Newhead;
-                list1=list1->next;
-            }
-            else{
-                Newhead=list2;
-                tail = Newhead;
-                list2=list2->next;
-            }
+        if(list1->val<list2->val){
+            Newhead=tail=list1;
+            list1=list1->next;
+            tail->next=nullptr;
         }
         else{
-            if(list1) return list1;
-            else return list2;
+            Newhead=tail=list2;
+            list2=list2->next;
+            tail->next=NULL;
         }
         while(list1 &&list2){
             if(list1->val < list2->val){
                 tail->next = list1;
-                tail=tail->next;
                 list1= list1->next;
+                tail= tail->next;
+                tail->next =NULL;
             }
             else{
                 tail->next = list2;
-                tail=tail->next;
                 list2 = list2->next;
+                tail=tail->next;
+                tail->next=NULL;
             }
         }
         if(list2==NULL) tail->next = list1;

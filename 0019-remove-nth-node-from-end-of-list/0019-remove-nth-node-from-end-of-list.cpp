8340c93810147905bfd030dfed4750de6ed81f99
MIRIYALA TARUN KUMAR <83545474+miriyalatarunkumar@users.noreply.github.com>
2023-05-29 22:15:35 +0530

    Time: 3 ms (82.12%), Space: 10.7 MB (62.18%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,6 +11,7 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        /*    
             ListNode* p=head;
             ListNode* q=head;
             int N=0;
@@ -19,6 +20,7 @@ public:
                 p=p->next;
             }
             int delNodePrev = N-n;
+        // If n ==N, i.e, to delete the 1st element
             if(N-n == 0) return head->next;
             for(int i = 0; i< delNodePrev-1; i++){
                 q=q->next;
@@ -26,5 +28,22 @@ public:
             p = q->next;
             q->next = p-> next;
             return head;
+        */
+        ListNode* start = new ListNode();
+        start->next = head;
+        ListNode* slow = start;
+        ListNode* fast = start;
+        for(int i = 0;i<n;i++){
+            fast = fast->next;
+        }
+        while(fast->next !=NULL){ // i.e, fast is not the last element
+            slow = slow->next;
+            fast = fast->next;
+        }
+        // if n=N, then slow won't move and will be at start only
+        slow->next = slow->next->next;
+        
+        return start->next;
+        // i.e, head in normal case and head->next in case of n=N because slow->next points to head->next, i.e, start-> next points to head->next which is new head.
     }
 };

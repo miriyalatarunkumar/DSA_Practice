ff4f7cf26c2f607b465cd43dd397ee6ec2b2ef49
MIRIYALA TARUN KUMAR <83545474+miriyalatarunkumar@users.noreply.github.com>
2023-05-30 00:22:43 +0530

    Time: 51 ms (15.09%), Space: 71.5 MB (40.64%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode * ans = new ListNode();
+        ListNode* temp=ans;
+        int num = l1->val+l2->val;
+        l1=l1->next;
+        l2=l2->next;
+        ans->val = num%10;
+        num = num/10;
+        while(l1 && l2){
+            ListNode * newNode = new ListNode();
+            num += l1->val+l2->val;
+            l1=l1->next;
+            l2=l2->next;
+            newNode->val = num%10;
+            temp->next = newNode;
+            temp=temp->next;
+            num = num/10;
+        }
+        while(l1){
+            ListNode * newNode = new ListNode();
+            num += l1->val;
+            l1=l1->next;
+            newNode->val = num%10;
+            temp->next = newNode;
+            temp=temp->next;
+            num = num/10;
+        }
+        while(l2){
+            ListNode * newNode = new ListNode();
+            num += l2->val;
+            l2=l2->next;
+            newNode->val = num%10;
+            temp->next = newNode;
+            temp=temp->next;
+            num = num/10;
+        }
+        while(num){
+            ListNode * newNode = new ListNode();
+            newNode->val = num%10;
+            temp->next = newNode;
+            temp=temp->next;
+            num = num/10;
+        }
+        return ans;
+        
+    }
+};

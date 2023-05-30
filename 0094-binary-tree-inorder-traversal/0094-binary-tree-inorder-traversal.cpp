982c8809dd07b9e2e4f1d8b22322a30419dad459
MIRIYALA TARUN KUMAR <83545474+miriyalatarunkumar@users.noreply.github.com>
2023-05-30 14:06:11 +0530

    Time: 0 ms (100.00%), Space: 10 MB (8.86%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -19,8 +19,22 @@ private:
     }
 public:
     vector<int> inorderTraversal(TreeNode* root) {
+        
+        /*
         vector<int> v;
         ioT(root,v);
         return v;
+        
+        */
+    
+        vector<int> ans;
+        if (root == NULL) return ans;
+        vector<int> left = inorderTraversal(root->left);
+        ans.insert(ans.end(), left.begin(), left.end());
+        ans.push_back(root->val);
+        vector<int> right = inorderTraversal(root->right);
+        ans.insert(ans.end(), right.begin(), right.end());
+        return ans;
+
     }
 };

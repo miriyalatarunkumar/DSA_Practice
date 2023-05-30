b3eff631a702954eeeba299da3bb98cd6ee539c4
MIRIYALA TARUN KUMAR <83545474+miriyalatarunkumar@users.noreply.github.com>
2023-05-30 14:12:52 +0530

    Time: 4 ms (26.82%), Space: 10 MB (9.48%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -19,8 +19,24 @@ private:
     }
 public:
     vector<int> preorderTraversal(TreeNode* root) {
+        
+        /*
+        
         vector<int> v;
         preoT(root,v);
         return v;
+        
+        */
+        
+        vector<int> ans;
+        if (root == NULL) return ans;
+        ans.push_back(root->val);
+        vector<int> left = preorderTraversal(root->left);
+        ans.insert(ans.end(), left.begin(), left.end());
+        vector<int> right = preorderTraversal(root->right);
+        ans.insert(ans.end(), right.begin(), right.end());
+        return ans;
+        
+        
     }
 };

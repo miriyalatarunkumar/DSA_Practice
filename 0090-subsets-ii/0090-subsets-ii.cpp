899da78c4dfd0e1307f74098e2fc4c4891f106e5
MIRIYALA TARUN KUMAR <83545474+miriyalatarunkumar@users.noreply.github.com>
2023-05-31 12:58:23 +0530

    Time: 11 ms (17.76%), Space: 15.9 MB (22.38%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -12,12 +12,23 @@ class Solution {
             }
         }
     }
+    void fun_opti(int i,vector<int> nums,vector<vector<int>>& ans,vector<int>& v){
+        if(i<nums.size()){
+            v.push_back(nums[i]);
+            fun(i+1,nums,ans,v);
+            v.pop_back();
+            while( i+1 < nums.size() && nums[i] == nums[i+1]) i++;
+            fun(i+1,nums,ans,v);
+        }
+        else ans.push_back(v);
+    }
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> v;
         sort(nums.begin(),nums.end());
-        fun(0,nums,ans,v);
+        // fun(0,nums,ans,v);
+        fun_opti(0,nums,ans,v);
         return ans;
     }
 };

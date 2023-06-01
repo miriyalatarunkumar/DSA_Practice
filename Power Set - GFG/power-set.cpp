2302eb31f2c223ce08f79d07f828660f209582f5
MIRIYALA TARUN KUMAR <83545474+miriyalatarunkumar@users.noreply.github.com>
2023-06-01 10:55:30 +0530

    Added solution - LeetHub

--- Power Set - GFG/power-set.cpp
+++ Power Set - GFG/power-set.cpp
@@ -4,9 +4,25 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution{
+    private:
+        void fun(int i, int n, string s, vector<string> &ans, string &sub){
+            if(i<n){
+                sub += s[i];
+                fun(i+1,n,s,ans,sub);
+                sub.pop_back();
+                fun(i+1,n,s,ans,sub);
+            }
+            else{
+                if(sub.size()>0){
+		            ans.push_back(sub);
+                }
+            }
+        }
 	public:
 		vector<string> AllPossibleStrings(string s){
 		    // Code here
+		    
+		    /*
 		    vector<string> ans;
 		    int n = s.size();
 		    for(int num =0; num<(1<<n);num++){
@@ -22,6 +38,16 @@ class Solution{
 		    }
 		    sort(ans.begin(),ans.end());
 		    return ans;
+		    
+		    */
+		    
+		    vector<string> ans;
+		    int n = s.size();
+		    string sub = "";
+		    fun(0,n,s,ans,sub);
+		    sort(ans.begin(),ans.end());
+		    return ans;
+		    
 		}
 };
 

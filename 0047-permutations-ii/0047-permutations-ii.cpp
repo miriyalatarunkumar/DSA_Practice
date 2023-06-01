37cd557a1fadff1e157256572625e9e095ed6dce
MIRIYALA TARUN KUMAR <83545474+miriyalatarunkumar@users.noreply.github.com>
2023-06-01 16:26:27 +0530

    Time: 896 ms (5.01%), Space: 8.4 MB (88.84%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,15 +1,16 @@
 class Solution {
 private:
     void fun(int pos, vector<int>& arr, int n, vector<vector<int>>& ans) {
-        if (pos == n - 1) {
+        if(pos==n-1){
             if(find(ans.begin(),ans.end(),arr)==ans.end()){
-                ans.push_back(arr);   
-            }
-        } else {
-            for (int i = pos; i < n; i++) {
-                swap(arr[i], arr[pos]);
-                fun(pos + 1, arr, n, ans);
-                swap(arr[i], arr[pos]);
+                    ans.push_back(arr);   
+                }
+        }
+        else{
+            for(int i = pos; i<n; i++){
+                swap(arr[i],arr[pos]);
+                fun(pos+1,arr,n,ans);
+                swap(arr[i],arr[pos]);
             }
         }
     }
@@ -18,9 +19,9 @@ public:
     vector<vector<int>> permuteUnique(vector<int>& arr) {
         int n = arr.size();
         vector<vector<int>> ans;
-        sort(arr.begin(), arr.end());
+        //sort(arr.begin(), arr.end());
         fun(0, arr, n, ans);
-        sort(ans.begin(), ans.end());
+        //sort(ans.begin(), ans.end());
         return ans;
     }
 };

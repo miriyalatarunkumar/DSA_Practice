b73bc09302f02580e3ac0a4b0ed9b82074430de0
MIRIYALA TARUN KUMAR <83545474+miriyalatarunkumar@users.noreply.github.com>
2023-06-05 19:19:52 +0530

    Time: 4 ms (95.27%), Space: 9.9 MB (91.90%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
+    // Returns the delta Y.
+    int getYDiff(vector<int>& a, vector<int>& b) {
+        return a[1] - b[1];
+    }
+    
+    // Returns the delta X.
+    int getXDiff(vector<int>& a, vector<int>& b) {
+        return a[0] - b[0];
+    }
+    
     bool checkStraightLine(vector<vector<int>>& coordinates) {
-        int x1 = coordinates[0][0], y1 = coordinates[0][1], x2 = coordinates[1][0], y2 = coordinates[1][1];
-        int isxZero = 1;
-        for(auto it : coordinates){
-            if(it[0]!= x1) isxZero = 0;
-        }
-        if ( isxZero == 1) return true;
-        float m = (float)(y2 - y1) / (float)(x2 - x1);
-        float c = (float) y1 - m*x1;
-        for(int i = 2; i<coordinates.size();i++){
-            int x = coordinates[i][0];
-            int y = coordinates[i][1];
-            if(y != (m*x)+c) return false;
+        int deltaY = getYDiff(coordinates[1], coordinates[0]);
+        int deltaX = getXDiff(coordinates[1], coordinates[0]);
+        
+        for (int i = 2; i < coordinates.size(); i++) {
+            // Check if the slope between points 0 and i, is the same as between 0 and 1.
+            if (deltaY * getXDiff(coordinates[i], coordinates[0])
+                != deltaX * getYDiff(coordinates[i], coordinates[0])) {
+                return false;
+            }
         }
         return true;
     }

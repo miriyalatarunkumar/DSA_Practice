42f22825a408f4e4bedc109d2dcd7bba297b689f
MIRIYALA TARUN KUMAR <83545474+miriyalatarunkumar@users.noreply.github.com>
2023-06-13 15:57:50 +0530

    Time: 112 ms (61.11%), Space: 25.9 MB (79.35%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
+        /*
         int n = grid.size();
         int ans = 0,k=0;
         for(int i =0; i<n; i++){
@@ -9,6 +10,26 @@ public:
                     if(grid[i][k] != grid[k][j]) break;
                 }
                 if(k==n) ans++;
+            }
+        }
+        return ans;
+        */
+        
+        vector<vector<int>> grid2=grid;
+        for(int i=0; i<grid.size(); i++)
+        {
+            for(int j=i; j<grid[0].size(); j++)
+            {
+                swap(grid[i][j],grid[j][i]);
+            }
+        }
+        int ans=0;
+        for(int i=0;i<grid2.size(); i++)
+        {
+            for(int j=0;j<grid.size(); j++)
+            {
+                if(grid2[i]==grid[j]) // comparing the row vectors in two matrices
+                    ans++;
             }
         }
         return ans;

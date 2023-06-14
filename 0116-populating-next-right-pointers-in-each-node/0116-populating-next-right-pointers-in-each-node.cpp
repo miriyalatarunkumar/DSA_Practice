05e694d90704aac821a9d26dae729d086dcbc3dd
MIRIYALA TARUN KUMAR <83545474+miriyalatarunkumar@users.noreply.github.com>
2023-06-14 19:19:51 +0530

    Time: 15 ms (95.27%), Space: 17 MB (60.61%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -19,6 +19,7 @@ public:
 class Solution {
 public:
     Node* connect(Node* root) {
+        /*
         if(root == NULL || (root->right == NULL && root -> left == NULL)) return root;
         queue<Node*>q;
         q.push(root);
@@ -33,6 +34,22 @@ public:
                 if(!q.empty())p->next = q.front();
             }
             q=q1;
+        }
+        return root;
+        */
+        if(!root) return nullptr;
+        queue<Node*> q;
+        q.push(root);        
+        while(size(q)) {
+            Node* rightNode = nullptr;         // set rightNode to null initially
+            for(int i = size(q); i; i--) {     // traversing each level
+                auto cur = q.front(); q.pop(); // pop a node from current level and,
+                cur -> next = rightNode;    // set its next pointer to rightNode
+                rightNode = cur;            // update rightNode as cur for next iteration
+                if(cur -> right)            // if a child exists
+                    q.push(cur -> right),   // IMP: push right first to do right-to-left BFS
+                    q.push(cur -> left);    // then push left
+            }
         }
         return root;
     }

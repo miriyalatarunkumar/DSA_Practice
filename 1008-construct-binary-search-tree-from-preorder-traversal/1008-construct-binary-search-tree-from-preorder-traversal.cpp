345f2e4a6cc6420addff561856a033a6f59cd596
MIRIYALA TARUN KUMAR <83545474+miriyalatarunkumar@users.noreply.github.com>
2023-06-14 21:54:02 +0530

    Time: 8 ms (40.98%), Space: 12.8 MB (90.73%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -28,10 +28,43 @@ private:
     }
 public:
     TreeNode* bstFromPreorder(vector<int>& preorder) {
+        
+        /*
         TreeNode* root = new TreeNode(preorder[0]);
         if(preorder.size()==1) return root;
         for(int i =1; i<preorder.size(); i++){
             insert(root, preorder[i]);
+        }
+        return root;
+        */
+        
+        stack<TreeNode*> s;
+        TreeNode* root = new TreeNode(preorder[0]);
+        if(preorder.size()==1) return root;
+        TreeNode *p = root;
+        int i = 1;
+        while(i<preorder.size()){
+            if(preorder[i]<p->val){
+                s.push(p);
+                p->left = new TreeNode(preorder[i++]);
+                p = p->left;
+            }
+            else{
+                if(!s.empty()){
+                    if(preorder[i]<s.top()->val){
+                        p->right = new TreeNode(preorder[i++]);
+                        p = p->right;
+                    }
+                    else{
+                        p = s.top();
+                        s.pop();
+                    }
+                }
+                else{
+                    p->right = new TreeNode(preorder[i++]);
+                    p = p->right;
+                }
+            }
         }
         return root;
     }

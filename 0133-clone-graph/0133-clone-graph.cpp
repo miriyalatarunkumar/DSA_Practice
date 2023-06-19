8ca8ea4235248f11b5634798fcf4e30da965948d
MIRIYALA TARUN KUMAR <83545474+miriyalatarunkumar@users.noreply.github.com>
2023-06-19 20:04:07 +0530

    Time: 5 ms (62.34%), Space: 8.9 MB (55.26%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -21,8 +21,25 @@ public:
 
 class Solution {
 public:
-    unordered_map<int,Node*>m;
+    Node* dfs(Node* cur,unordered_map<Node*,Node*>& mp)
+    {
+        Node* clone=new Node(cur->val);
+        mp[cur]=clone;
+            for(auto it:cur->neighbors)
+            {
+                if(mp.find(it)!=mp.end())   //already clone and stored in map
+                {
+                    clone->neighbors.push_back(mp[it]);    //directly push back the clone node from map to neigh
+                }
+                else
+                    clone->neighbors.push_back(dfs(it,mp));
+            }
+            return clone;
+    }
+    
+    // unordered_map<int,Node*>m;
     Node* cloneGraph(Node* node) {
+        /*
         if(!node)return node;
         Node* root = NULL;
         if(!m.count(node->val)){
@@ -36,5 +53,15 @@ public:
             }
         }
         return root;
+        */
+        unordered_map<Node*,Node*> mp;
+        if(node==NULL)
+            return NULL;
+        if(node->neighbors.size()==0)   //if only one node present no neighbors
+        {
+            Node* clone= new Node(node->val);
+            return clone; 
+        }
+        return dfs(node,mp);
     }
 };

355633df2c4598fc331c7670c007f6c6c1458558
MIRIYALA TARUN KUMAR <83545474+miriyalatarunkumar@users.noreply.github.com>
2023-07-27 10:54:27 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (74.36%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,35 +1,24 @@
 class Solution {
-    void fun(int i,vector<int> nums,vector<vector<int>>& ans,vector<int>& v){
-        if(i<nums.size()){
-            v.push_back(nums[i]);
-            fun(i+1,nums,ans,v);
-            v.pop_back();
-            fun(i+1,nums,ans,v);
+private:
+    void fun(vector<int>& nums, int n, vector<int> &v, vector<vector<int>> &ans, int i){
+        if(i==n){
+            ans.push_back(v);
+            return;
         }
-        else {
-            if( find(ans.begin(),ans.end(),v) == ans.end()){
-                ans.push_back(v);  
-            }
-        }
-    }
-    void fun_opti(int i,vector<int> nums,vector<vector<int>>& ans,vector<int>& v){
-        if(i<nums.size()){
-            v.push_back(nums[i]);
-            fun_opti(i+1,nums,ans,v);
-            v.pop_back();
-            while( i+1 < nums.size() && nums[i] == nums[i+1]) i++;
-            fun_opti(i+1,nums,ans,v);
-        }
-        else ans.push_back(v);
+        v.push_back(nums[i]);
+        fun(nums, n, v, ans, i+1);
+        v.pop_back();
+        int num = nums[i];
+        while(i < n && nums[i]==num){i++;}
+        fun(nums, n, v, ans, i);
     }
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
         vector<int> v;
-        sort(nums.begin(),nums.end());
-        //fun(0,nums,ans,v);
-        fun_opti(0,nums,ans,v);
-        sort(ans.begin(),ans.end()); // not asked for it, but can be done
+        int n = nums.size();
+        fun(nums, n, v, ans, 0);
         return ans;
     }
 };

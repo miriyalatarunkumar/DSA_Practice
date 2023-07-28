652cb3fde2f76a203445c9e11b5c4a7435237a6b
MIRIYALA TARUN KUMAR <83545474+miriyalatarunkumar@users.noreply.github.com>
2023-07-28 17:07:31 +0530

    Added solution - LeetHub

---
+++ Lowest Common Ancestor in a BST - GFG/lowest-common-ancestor-in-a-bst.cpp
@@ -0,0 +1,140 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+// Function to Build Tree
+
+
+// } Driver Code Ends
+//Function to find the lowest common ancestor in a BST.
+class Solution{
+    private:
+        void fun(Node *a, int n, stack<Node*> &s){
+            while(a){
+                    s.push(a);
+                    if(a->data==n)break;
+                    if(a->data>n)a=a->left;
+                    else a=a->right;
+                }
+        }
+    public:
+        Node* LCA(Node *root, int n1, int n2)
+        {
+            // code here
+            stack<Node*> s1, s2;
+            fun(root, n1, s1);
+            fun(root, n2, s2);
+            while(s1.size()!=s2.size()){
+                if(s1.size()>s2.size()){
+                    s1.pop();
+                }else s2.pop();
+            }
+            while(!s1.empty()){
+                if(s1.top()==s2.top())return s1.top();
+                else{
+                    s1.pop();
+                    s2.pop();
+                }
+            }
+            return NULL;
+        }
+
+};
+
+
+//{ Driver Code Starts.
+
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+
+int main()
+{
+
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+        int l , h;
+		getline(cin,s);
+		scanf("%d ",&l);
+		scanf("%d ",&h);
+        Node* root = buildTree(s);
+        Solution sol;
+        int ans = sol.LCA(root, l, h)->data;
+        cout<<ans<<endl;
+    }
+    return 1;
+}
+// } Driver Code Ends

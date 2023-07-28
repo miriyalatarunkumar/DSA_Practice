1ae281f71fe0b33ad37db5d75f9df8551ca9b376
MIRIYALA TARUN KUMAR <83545474+miriyalatarunkumar@users.noreply.github.com>
2023-07-28 22:43:39 +0530

    Time: 66 ms (100.00%), Space: 49.2 MB (84.45%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,5 +1,6 @@
 class Solution {
 private:
+    /*
     bool ip(string s){
         int l = 0;
         int r = s.length()-1;
@@ -23,11 +24,36 @@ private:
         }
         
     }
+    */
+    
+    void dfs(vector<vector<string>> &result, string &s, int start, vector<string> &currentList, vector<vector<bool>> &dp) {
+        if (start >= s.length()) result.push_back(currentList);
+        for (int end = start; end < s.length(); end++) {
+            if (s[start] == s[end] && (end - start <= 2 || dp[start + 1][end - 1])) {
+                dp[start][end] = true;
+                currentList.push_back(s.substr(start, end - start + 1));
+                dfs(result, s, end + 1, currentList, dp);
+                currentList.pop_back();
+            }
+        }
+    }
+    
 public:
     vector<vector<string>> partition(string s) {
+        /*
         vector<vector<string>> ans;
         vector<string> a;
         fun(s, a, ans, 0);
         return ans;
+        */
+        
+        // DP approach
+        
+        int len = s.length();
+        vector<vector<bool>> dp (len, vector <bool> (len, false));
+        vector<vector<string>> result;
+        vector<string> currentList;
+        dfs(result, s, 0, currentList, dp);
+        return result;
     }
 };

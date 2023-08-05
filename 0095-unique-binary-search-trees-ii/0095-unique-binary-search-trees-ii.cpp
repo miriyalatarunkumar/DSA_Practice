8b8de43563b92f7b5dc33e41e1d2fe009934e342
MIRIYALA TARUN KUMAR <83545474+miriyalatarunkumar@users.noreply.github.com>
2023-08-05 22:23:29 +0530

    Time: 13 ms (87.58%), Space: 12.5 MB (86.56%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
@@ -10,23 +10,28 @@
  * };
  */
 class Solution {
-    vector<TreeNode*> generate(int left, int right){
-        if(left>right) return {NULL};
+    vector<TreeNode*> generate(int left, int right, map<pair<int, int>, vector<TreeNode*>>& dp){
         vector<TreeNode*> ans;
+        if(left>right){
+            ans.push_back(NULL);
+            return ans;
+        }
+        if(dp.count({left,right})) return dp[{left,right}];
         for(int i=left; i<=right; i++){
-            auto lsubtrees = generate(left, i-1);
-            auto rsubtrees = generate(i+1, right);
+            auto lsubtrees = generate(left, i-1, dp);
+            auto rsubtrees = generate(i+1, right, dp);
             for(auto l : lsubtrees){
                 for(auto r : rsubtrees){
-                    TreeNode* t = new TreeNode(i, l, r);
-                    ans.push_back(t);
+                    TreeNode* root = new TreeNode(i, l, r);
+                    ans.push_back(root);
                 }
             }
         }
-        return ans;
+        return dp[{left,right}] = ans;
     }
 public:
     vector<TreeNode*> generateTrees(int n) {
-        return generate(1, n);
+         map<pair<int, int>, vector<TreeNode*>> dp;
+        return generate(1, n, dp);
     }
 };

8b9e439bbe988076d6e2685675163ef4383b7a0f
MIRIYALA TARUN KUMAR <83545474+miriyalatarunkumar@users.noreply.github.com>
2023-08-06 01:14:22 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (35.98%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -11,6 +11,13 @@ private:
 public:
     int climbStairs(int n) {
         vector<int> dp(n+1, -1);
-        return fun(n, n, dp);
+        dp[0]=1;
+        for(int i = 1; i<=n; i++){
+            int first = dp[i-1];
+            int second = 0;
+            if(i-2>=0) second = dp[i-2];
+            dp[i]=first+second;
+        }
+        return dp[n];
     }
 };

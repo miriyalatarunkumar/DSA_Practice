6030860be7d158ec3debd658d6dcfd6fbf7372bb
MIRIYALA TARUN KUMAR <83545474+miriyalatarunkumar@users.noreply.github.com>
2023-08-06 23:47:13 +0530

    Time: 5 ms (86.13%), Space: 6 MB (75.59%) - LeetHub

---
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countHousePlacements(int n) {
+        int mod = 1e9+7;
+        long long prev1 = 1; // putting house at index 1
+        long long prev2 = 0;
+        for(int i=2; i<=n; i++){
+            long long pick = 1 + prev2;
+            long long npick = prev1;
+            long long curr = (pick + npick) % mod; 
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        prev1 = (prev1 + 1) % mod; // no house at index 1
+        return (prev1 * prev1) % mod;
+    }
+};

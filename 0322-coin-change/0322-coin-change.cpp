135e4ad484152d9bef83f759bd3ce56faba036ab
MIRIYALA TARUN KUMAR <83545474+miriyalatarunkumar@users.noreply.github.com>
2023-08-07 21:41:29 +0530

    Time: 67 ms (90.51%), Space: 14.5 MB (70.39%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,30 +1,26 @@
 class Solution {
+private:
+    int fun(vector<int> &coins, int amount, int i){
+        if(amount == 0) return 0;
+        if(i<0) return 1e9;
+        int pick = 1e9;
+        if(amount>=coins[i]) pick = 1+fun(coins, amount - coins[i], i);
+        int npick = fun(coins, amount, i-1);
+        return min(pick, npick);
+    }
 public:
-    int solve(vector<int>&coins, int amount,vector<int>&dp){
-        if(amount<0){
-            return INT_MAX;
-        }
-        if(amount==0){
-            return 0;
-        }
-        if(dp[amount]!=-1){
-            return dp[amount];
-        }
-        int ans = INT_MAX;
-        for(int i=0;i<coins.size();i++){
-            int res = solve(coins, amount-coins[i],dp);
-            if(res!=INT_MAX){
-                ans = min(ans, res+1);
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, amount + 1); 
+        // initialize the dp array with an invalid number which is amount + 1 
+        // because the maximum number of coins that we can use to form the amount is amount itself, by using 1 denomination. 
+        dp[0] = 0; // no coin is needed to form amount 0
+        for(int i=1; i<=amount; i++){
+            for(auto &coin: coins){
+                if(coin <= i){
+                    dp[i] = min(dp[i], dp[i-coin] + 1);
+                }
             }
         }
-        return dp[amount]=ans;
-    }
-    int coinChange(vector<int>& coins, int amount) {
-        vector<int>dp(amount+1,-1);
-        int ans = solve(coins, amount,dp);
-        if(ans == INT_MAX){
-            return -1;
-        }
-        return ans;
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 };

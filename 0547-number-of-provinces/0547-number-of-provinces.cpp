87865edddc28f6fbaf02f512138d4ec7a2ca546f
MIRIYALA TARUN KUMAR <83545474+miriyalatarunkumar@users.noreply.github.com>
2023-08-25 11:54:29 +0530

    Time: 33 ms (36.06%), Space: 15.9 MB (55.64%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -10,12 +10,33 @@ private:
 }
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
+        /*
         int ans =0, n=isConnected.size();
         vector<bool> visited(n,0);
         for(int i=0; i<n; i++){
             if(!visited[i]){
                 ans++;
                 dfs(i, isConnected, visited, n);
+            }
+        }
+        return ans;
+        */
+        
+        int ans =0, n=isConnected.size();
+        vector<bool> visited(n,0);
+        for(int i=0; i<n; i++){
+            if(!visited[i]){
+                ans++;
+                queue<int> q;
+                q.push(i);
+                while(!q.empty()){
+                    int k=q.front();
+                    visited[k]=1;
+                    q.pop();
+                    for(int j=0; j<n; j++){
+                        if(isConnected[k][j] && !visited[j]) q.push(j);
+                    }
+                }
             }
         }
         return ans;

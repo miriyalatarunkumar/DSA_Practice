80866766669e91293a903143726f27d14c0b4b13
MIRIYALA TARUN KUMAR <83545474+miriyalatarunkumar@users.noreply.github.com>
2023-09-02 19:44:31 +0530

    Time: 189 ms (51.68%), Space: 80.3 MB (59.09%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -2,7 +2,7 @@ class Solution {
     int fun(int i, string &s, unordered_set<string> &st, vector<int> &dp, int n){
         if(i==n) return 0;
         if(dp[i] != -1) return dp[i];
-        dp[i] = 1 + fun(i+1, s, st, dp, n);
+        dp[i] = 1 + fun(i+1, s, st, dp, n); // npick case
         for(int j=i; j<n; j++){
             if(st.count(s.substr(i,j-i+1))){
                 int pick = fun(j+1, s, st, dp, n);

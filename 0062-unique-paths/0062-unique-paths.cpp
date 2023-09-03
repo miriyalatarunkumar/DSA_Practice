3899856879af1e0925f693de7ea99ca953976b32
MIRIYALA TARUN KUMAR <83545474+miriyalatarunkumar@users.noreply.github.com>
2023-09-03 11:54:35 +0530

    Time: 2 ms (44.36%), Space: 6.1 MB (71.42%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<int> prev(n,0), curr(n,1);
-        for(int i=0; i<m; i++){
+        vector<int> prev(n,1), curr(n,1);
+        for(int i=1; i<m; i++){
             for(int j=1; j<n; j++){
                 curr[j]=curr[j-1]+prev[j];
             }

60d607580596f36276bade9bb88de51cf73cff23
MIRIYALA TARUN KUMAR <83545474+miriyalatarunkumar@users.noreply.github.com>
2023-09-04 14:35:48 +0530

    Time: 3 ms (97.79%), Space: 9.5 MB (15.01%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -10,13 +10,13 @@ class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
         // can be done by hashing using unordered_set.
-        if(!head) return NULL;
+        if(!head) return head;
         unordered_set<ListNode*> s;
         while(head){
             if(s.count(head))return head;
             s.insert(head);
             head=head->next;
         }
-        return NULL;
+        return head;
     }
 };

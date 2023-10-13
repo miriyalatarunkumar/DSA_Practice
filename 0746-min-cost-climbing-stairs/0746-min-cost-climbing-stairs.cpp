ea959b8d64c358ce47a965e275c6d9e098e1ae46
MIRIYALA TARUN KUMAR <83545474+miriyalatarunkumar@users.noreply.github.com>
2023-10-13 22:31:43 +0530

    Time: 10 ms (13.04%), Space: 14.1 MB (26.23%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -11,6 +11,14 @@ public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
         vector<int> dp(n+1, -1);
-        return fun(cost, n, dp);
+        // return fun(cost, n, dp);
+        dp[1]=0;
+        dp[2]=min(cost[0],cost[1]);
+        for(int i=3; i<=n; i++){
+            int p = cost[i-1]+dp[i-1];
+            int q = cost[i-2]+dp[i-2];
+            dp[i] = min(p,q);
+        }
+        return dp[n];
     }
 };

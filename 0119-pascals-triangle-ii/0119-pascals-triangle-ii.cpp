bd5b0616ccb86bdc45a299f7fe2198f8af3857bf
MIRIYALA TARUN KUMAR <83545474+miriyalatarunkumar@users.noreply.github.com>
2023-10-16 08:11:56 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (51.35%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
+        /*
         if(rowIndex == 0)return {1};
         if(rowIndex == 1)return {1,1};
         vector<int> prev = {1,1};
@@ -12,5 +13,12 @@ public:
             prev=curr;
         }
         return prev;
+        */
+        vector<int> A(rowIndex+1, 0);
+        A[0] = 1;
+        for(int i=1; i<rowIndex+1; i++)
+            for(int j=i; j>=1; j--)
+                A[j] += A[j-1];
+        return A;
     }
 };
